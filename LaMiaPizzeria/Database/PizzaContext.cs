207b07c325bc7c6b9a7f6f1e7c988ef5acb9e12c
thrudr <ariel.oliveiraesilva@gmail.com>
2023-05-18 15:42:57 +0200

    db-model pizza

--- LaMiaPizzeria/Database/PizzaContext.cs
+++ LaMiaPizzeria/Database/PizzaContext.cs
@@ -1,6 +1,15 @@
-﻿namespace LaMiaPizzeria.Database
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LaMiaPizzeria.Database
 {
-    public class PizzaContext
+    public class PizzaContext : DbContext
     {
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=SampaPizzariaDB;" +
+            "Integrated Security=True;TrustServerCertificate=True");
+        }
     }
 }
---
+++ LaMiaPizzeria/Models/Pizza.cs
@@ -0,0 +1,22 @@
+﻿namespace LaMiaPizzeria.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Imagine { get; set; }
+        public double Price { get; set; }
+
+
+        public Pizza() { }
+
+        public Pizza(string name, string description, string imagine, double price)
+        {
+            this.Name = name;
+            this.Description= description;
+            this.Imagine= imagine;
+            this.Price= price;
+        }
+    }
+}
