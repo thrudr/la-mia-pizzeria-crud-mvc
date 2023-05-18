9b430b622055f3a350d5cd4e71c93d04cf37969e
thrudr <ariel.oliveiraesilva@gmail.com>
2023-05-18 15:51:27 +0200

    model pizza completed

--- LaMiaPizzeria/Models/Pizza.cs
+++ LaMiaPizzeria/Models/Pizza.cs
@@ -1,11 +1,24 @@
-﻿namespace LaMiaPizzeria.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LaMiaPizzeria.Models
 {
     public class Pizza
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string Description { get; set; }
+
+        [Required]
         public string Imagine { get; set; }
+
+        [Required]
+        [Range(1,15000)]
         public double Price { get; set; }
 
 
